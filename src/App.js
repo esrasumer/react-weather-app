0d4818ccbc1d43fb4f16d6980963e86c8a4eb8fb
esrasumer <esrasumer28@gmail.com>
2022-10-18 00:15:25 +0300

    axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import axios from 'axios'
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-   esaez
-    </div>
-  );
+  const key = "0d433861b4105ae7027f2be177921252";
+
+    useEffect(() => {
+      async function getApi() {
+        try {
+          const response = await axios.get('');
+          console.log(response);
+        } catch (error) {
+          console.error(error)
+        }
+      }
+    })
+ return <div>Hello</div>
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,tsx,js,}",".",
-],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
