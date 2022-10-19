1b2ce0eb4e74e7bd1bbc176c33b8f1b8e0a6213c
esrasumer <esrasumer28@gmail.com>
2022-10-19 22:41:16 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
+import "./app.css";
 import axios from 'axios'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import City from "./City";
 
 function App() {
   const key = "0d433861b4105ae7027f2be177921252";
-
-    useEffect(() => {
-      async function getApi() {
-        try {
-          const response = await axios.get('');
-          console.log(response);
-        } catch (error) {
-          console.error(error)
-        }
+  const [search, setSearch] = useState("")
+  const [city, setCity] = useState()
+  useEffect(() => {
+    async function getApi() {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`);
+        console.log(response);
+        setCity(response.data);
+      } catch (error) {
+        console.error(error)
       }
-    })
- return <div>Hello</div>
+    }
+    getApi();
+  }, []) //array'in içi boş ise sadece uygulama ilk açıldığında çağırılır. //eğer içinde hook ya da değişken varsa o hook her render edeildiğinde çalışır
+  console.log(search)
+  return <div className="App">
+    <div>
+      <div className="mb-3 pt-0">
+        <input onChange={(e) => setSearch(e.target.value)} type="text" placeholder="Placeholder" className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full" />
+      </div>
+      <City />
+    </div>
+  </div>
 }
 
 export default App;
